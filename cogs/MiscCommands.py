1fa744c2aa3595d3fcdfcf8e7f62966fc111d2df
Ilya <54947380+I-Atlas@users.noreply.github.com>
2020-04-14 19:22:52 +0300

    Update MiscCommands

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -9,10 +9,27 @@ class MiscCommands(commands.Cog):
 
     @commands.command(name='color', aliases=['random_color'], pass_context=True)
     @commands.has_permissions()
-    async def ran_color(self, ctx):
+    async def random_color(self, ctx):
         clr = (random.randint(0, 16777215))
         emb = discord.Embed(description=f'Color generated : ``#{hex(clr)[2:]}``', colour=clr)
         await ctx.send(embed=emb)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def say(self, ctx, channel: discord.TextChannel, *, cnt):
+        await ctx.message.delete()  # delete your message
+        embed = discord.Embed(description=cnt, colour=0x00ff80)  # Generate embed message
+        await channel.send(embed=embed)  # Sending a message to the channel you specified
+
+    @commands.command()
+    @commands.has_permissions()
+    async def game(self, ctx, member: discord.Member = None):
+        await ctx.message.delete()  # delete your message
+        user = ctx.message.author if (member == None) else member
+        if user.activity:
+            await ctx.send(f"User {user.mention} plays **{user.activity}**")
+        else:
+            await ctx.send(f"User {user.mention} doesn’t play anything")
 
 
 def setup(bot):
