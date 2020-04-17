6521f6bb65ae21f6655ea0716d69c3b096ff112f
Ilya <54947380+I-Atlas@users.noreply.github.com>
2020-04-17 23:15:09 +0300

    Update AdminCommands
    Add @commands.Cog.listener() and remove pass_context argument

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -7,43 +7,53 @@ class AdminCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='ban', aliases=['banuser'], pass_context=True, invoke_without_command=True)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('- - - - -\nAdminCommands has been loaded!')
+
+    @commands.command(name='ban', aliases=['banuser'])
     @commands.has_permissions(administrator=True)
     async def ban_user_command(self, ctx, member: discord.Member = None, reason=None):
         await ctx.message.delete()  # delete your message
-        em = discord.Embed(description=f'User *{member.mention}* has been banned.', color=0xa9ffda)
-        await ctx.channel.send(embed=em)  # embed
+        embed = discord.Embed(description=f'User *{member.mention}* has been banned.', color=0xa9ffda)
+        await ctx.channel.send(embed=embed)  # embed
 
-    @commands.command(name='clear', aliases=['cl'], pass_context=True, invoke_without_command=True)
+    @commands.command(name='clear', aliases=['cl'])
     @commands.has_permissions(administrator=True)
     async def clear(self, ctx, amount=100):
+        """
+        A command that clear chat.
+        """
         await ctx.message.delete()  # delete your message
         await ctx.channel.purge(limit=amount)  # delete messages
-        em = discord.Embed(description=f'Deleted *{amount}* messages', color=0xa9ffda)
-        await ctx.send(embed=em)  # embed
+        embed = discord.Embed(description=f'Deleted *{amount}* messages', color=0xa9ffda)
+        await ctx.send(embed=embed)  # embed
         await asyncio.sleep(3)  # sleep timer
         await ctx.channel.purge(limit=1)  # delete bot message
 
-    @commands.command(name='move', aliases=['mv'], pass_context=True, invoke_without_command=True)
+    @commands.command(name='move', aliases=['mv'], invoke_without_command=True)
     @commands.has_permissions(administrator=True)
     async def move(self, ctx, arg=None, member: discord.Member = None):
+        """
+        A command that move members from one voice chat to another.
+        """
         channels = ctx.author.voice.channel.id
         await ctx.message.delete()
         if not channels:
-            em = discord.Embed(description=f'You must be in a voice channel', color=0xf75252)
-            await ctx.channel.send(embed=em, delete_after=10)
+            embed = discord.Embed(description=f'You must be in a voice channel', color=0xf75252)
+            await ctx.channel.send(embed=embed, delete_after=10)
             return
         if not arg:
-            em = discord.Embed(description=f'It is necessary to indicate where to move users', color=0xf75252)
-            await ctx.channel.send(embed=em, delete_after=10)
+            embed = discord.Embed(description=f'It is necessary to indicate where to move users', color=0xf75252)
+            await ctx.channel.send(embed=embed, delete_after=10)
             return
         voice = ctx.guild.voice_channels
         print(voice)
         try:
             vchannel = voice[int(arg) - 1]
         except:
-            em = discord.Embed(description=f'Invalid Argument', color=0xf75252)
-            await ctx.channel.send(embed=em, delete_after=10)
+            embed = discord.Embed(description=f'Invalid Argument', color=0xf75252)
+            await ctx.channel.send(embed=embed, delete_after=10)
             return
         if member == None:
             x = ctx.author.voice.channel.members
