bdac02d3a661897deca9cfed80caa27b084a2509
Ilya <54947380+I-Atlas@users.noreply.github.com>
2020-04-17 23:18:00 +0300

    Update MiscCommands
    Added @commands.Cog.listener() and removed pass_context argument

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -7,21 +7,25 @@ class MiscCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='color', aliases=['rc'], pass_context=True)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('- - - - -\nMiscCommands has been loaded!\n- - - - -')
+
+    @commands.command(name='color', aliases=['rc'])
     @commands.has_permissions()
     async def rnd_color(self, ctx):
         clr = (random.randint(0, 16777215))
         embed = discord.Embed(description=f'Color generated : ``#{hex(clr)[2:]}``', colour=clr)
         await ctx.send(embed=embed)
 
-    @commands.command(name='say', aliases=['s'], pass_context=True)
+    @commands.command(name='say', aliases=['s'])
     @commands.has_permissions(administrator=True)
     async def say(self, ctx, channel: discord.TextChannel, cnt):
         await ctx.message.delete()  # delete your message
         embed = discord.Embed(description=cnt, colour=0xa9ffda)  # Generate embed message
         await channel.send(embed=embed)  # Sending a message to the channel you specified
 
-    @commands.command(name='game', aliases=['g'], pass_context=True)
+    @commands.command(name='game', aliases=['g'])
     @commands.has_permissions()
     async def game(self, ctx, member: discord.Member = None):
         await ctx.message.delete()  # delete your message
