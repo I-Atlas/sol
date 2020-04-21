40e1dc96c7651725a139feb12b9f33124bbffa80
Iliya Bolotov <54947380+I-Atlas@users.noreply.github.com>
2020-04-21 20:51:04 +0300

    Update main file

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from discord.ext import commands, tasks
 
 load_dotenv()  # load .env file
 bot = commands.Bot(command_prefix='!')
-bot.remove_command("help")
+bot.remove_command('help')
 extensions = ['cogs.CommandEvents', 'cogs.AdminCommands', 'cogs.HelpCommands', 'cogs.MiscCommands']  # cogs
-list_of_status = ['Hello there!', 'Workspace: Sol', 'Working on 2990WX', 'Events loop']  # list of activities
+list_of_status = ['Hello there!', '🌌', 'Working on 2990WX', '☀', '🌻', '🚀']  # list of activities
 
 
-@tasks.loop(minutes=60)
-async def change_status():
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name=list_of_status[random.randint(0, 3)]))
+@tasks.loop(minutes=1)
+async def change_presence():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name=list_of_status[random.randint(0, 5)]))
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is online!', '\nID:', bot.user.id, flush=True)
-    change_status.start()
+    change_presence.start()
 
 
 if __name__ == '__main__':
