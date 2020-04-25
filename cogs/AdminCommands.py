c2785dce302425aa376fa63becb1d291eb67a4fb
Iliya Bolotov <54947380+I-Atlas@users.noreply.github.com>
2020-04-25 22:25:34 +0300

    Update AdminCommands

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -6,12 +6,14 @@ from discord.ext import commands
 class AdminCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.desc = "Ban user on server"
+        self.usage = "ban/bn [user] [reason]"
 
     @commands.Cog.listener()
     async def on_ready(self):
         print('- - - - -\nAdminCommands has been loaded!')
 
-    @commands.command(name='ban', aliases=['banuser'])
+    @commands.command(name='ban', aliases=['bn'])
     @commands.has_permissions(administrator=True)
     async def ban(self, ctx, member: discord.Member = None, reason=None):
         """
@@ -19,24 +21,28 @@ class AdminCommands(commands.Cog):
         """
         await ctx.message.delete()
         if member is None:
-            embed = discord.Embed(title='Error', description=f'Please specify someone to ban.', color=0xf75252, delete_after=10)
+            embed = discord.Embed(title='⚠', description=f'Please specify someone to ban.', color=0xf75252, delete_after=10)
             await ctx.send(embed=embed)
         elif member is ctx.message.author:
-            embed = discord.Embed(title='Error', description=f'You cannot ban yourself.', color=0xf75252, delete_after=10)
+            embed = discord.Embed(title='⚠', description=f'You cannot ban yourself.', color=0xf75252, delete_after=10)
             await ctx.send(embed=embed)
         else:
             if reason is None:
-                embed = discord.Embed(title='Ban', description=f'{ctx.author.mention} banned {member.mention}.', color=0xa9ffda, delete_after=10)
+                embed = discord.Embed(title=None, description=f'{member.mention} banned by {ctx.author.mention}.', color=0xa9ffda)
+                embed.set_author(name=" | Ban", icon_url=self.bot.user.avatar_url)
+                embed.set_footer(text=f" | Banned by {ctx.author}.", icon_url=ctx.author.avatar_url)
                 await ctx.send(embed=embed)
                 try:
                     embed = discord.Embed(title='Ban', description=f'You has been banned on server {ctx.guild.name}.', color=0xa9ffda)
-                    await ctx.send(embed=embed)
+                    await member.send(embed=embed)
                 except Exception:
                     print('Ban error')
                 finally:
                     await ctx.guild.ban(member)
             elif reason is not None:
-                embed = discord.Embed(title='Ban', description=f'{ctx.author.mention} banned {member.mention} with reason: {reason}.', color=0xa9ffda, delete_after=10)
+                embed = discord.Embed(title=None, description=f'{member.mention} banned by {ctx.author.mention} with reason: {reason}.', color=0xa9ffda)
+                embed.set_author(name=" | Ban", icon_url=self.bot.user.avatar_url)
+                embed.set_footer(text=f" | Banned by {ctx.author}.", icon_url=ctx.author.avatar_url)
                 await ctx.send(embed=embed)
                 try:
                     embed = discord.Embed(title='Ban', description=f'You has been banned on server {ctx.guild.name} with reason: {reason}.', color=0xa9ffda)
@@ -54,14 +60,16 @@ class AdminCommands(commands.Cog):
         """
         await ctx.message.delete()
         if member is None:
-            embed = discord.Embed(title='Error', description=f'Please specify someone to kick.', color=0xf75252, delete_after=10)
+            embed = discord.Embed(title='⚠', description=f'Please specify someone to kick.', color=0xf75252, delete_after=10)
             await ctx.send(embed=embed)
         elif member is ctx.message.author:
-            embed = discord.Embed(title='Error', description=f'You cannot kick yourself.', color=0xf75252, delete_after=10)
+            embed = discord.Embed(title='⚠', description=f'You cannot kick yourself.', color=0xf75252, delete_after=10)
             await ctx.send(embed=embed)
         else:
             if reason is None:
-                embed = discord.Embed(title='Kick', description=f'{ctx.author.mention} kicked {member.mention}.', color=0xa9ffda, delete_after=10)
+                embed = discord.Embed(title=None, description=f'{member.mention} kicked by {ctx.author.mention}.', color=0xa9ffda)
+                embed.set_author(name=" | Kick", icon_url=self.bot.user.avatar_url)
+                embed.set_footer(text=f" | Kicked by {ctx.author}.", icon_url=ctx.author.avatar_url)
                 await ctx.send(embed=embed)
                 try:
                     embed = discord.Embed(title='Kick', description=f'You has been kicked from the server {ctx.guild.name}.', color=0xa9ffda)
@@ -71,7 +79,9 @@ class AdminCommands(commands.Cog):
                 finally:
                     await ctx.guild.kick(member)
             elif reason is not None:
-                embed = discord.Embed(title='Kick', description=f'{ctx.author.mention} kicked {member.mention} with reason: {reason}.', color=0xa9ffda, delete_after=10)
+                embed = discord.Embed(title=None, description=f'{member.mention} kicked by {ctx.author.mention} with reason: {reason}.', color=0xa9ffda)
+                embed.set_author(name=" | Kick", icon_url=self.bot.user.avatar_url)
+                embed.set_footer(text=f" | Kicked by {ctx.author}.", icon_url=ctx.author.avatar_url)
                 await ctx.send(embed=embed)
                 try:
                     embed = discord.Embed(title='Kick', description=f'You has been kicked from the server {ctx.guild.name} with reason: {reason}.', color=0xa9ffda)
