e98d3ac8d3cd0b988a9d0e8b9dfe99e9c16706f6
Iliya Bolotov <54947380+I-Atlas@users.noreply.github.com>
2020-05-31 16:30:03 +0300

    Add files via upload

---
+++ cogs/fun/tts.py
@@ -0,0 +1,26 @@
+# Imports
+import discord
+from discord.ext import commands
+
+
+# Main Command Class
+class Tts(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.desc = "Sends tts message."
+        self.usage = "tts [text]"
+
+    @commands.command(name='tts', aliases=['t'])
+    @commands.has_permissions()
+    async def tts(self, ctx, *, text):
+        if not text:
+            await ctx.send("Please specify something to say.")
+        try:
+            await ctx.send(text, tts=True)
+        except:
+            await ctx.send("I don't have permission to send TTS messages.")
+
+
+# Link to bot
+def setup(bot):
+    bot.add_cog(Tts(bot))
