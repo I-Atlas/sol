825b3acdd711617ed29b27a3c35cd044597f6a72
Iliya Bolotov <54947380+I-Atlas@users.noreply.github.com>
2020-06-01 22:16:23 +0300

    New command "avatar"
    New command "avatar" which opens user avatar and update for "stats" command

--- cogs/help/stats.py
+++ cogs/help/stats.py
@@ -1,12 +1,7 @@
 # Imports
-import os
 import platform
 import discord
-from dotenv import load_dotenv
 from discord.ext import commands
-
-# Dotenv
-load_dotenv()
 
 
 # Main Command Class
@@ -19,23 +14,20 @@ class Stats(commands.Cog):
     @commands.command(name='stats', aliases=['st'])
     @commands.has_permissions()
     async def stats(self, ctx):
-        """
-        A command that displays bot stats.
-        """
-        number_of_servers = len(self.bot.guilds)
-        number_of_members = len(set(self.bot.get_all_members()))
+        number_of_servers = str(len(self.bot.guilds))
+        number_of_members = str(len(set(self.bot.get_all_members())))
+        author_id = '<@271684584863825920>'
 
-        embed = discord.Embed(title=f'{self.bot.user.name} Stats', description='\uFEFF', color=ctx.author.color,
-                              timestamp=ctx.message.created_at)
+        embed = discord.Embed(title=f'{self.bot.user.name} Stats', color=ctx.author.color)
 
         embed.add_field(name='Python version', value=platform.python_version())
         embed.add_field(name='discord.py version', value=discord.__version__)
         embed.add_field(name='Total guilds', value=number_of_servers)
         embed.add_field(name='Total users', value=number_of_members)
-        embed.add_field(name='Bot developers', value=os.getenv('AUTHOR'))
+        embed.add_field(name='Bot developers', value=author_id)
 
-        embed.set_footer(text=f'Hello, world! | {self.bot.user.name}')
-        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+        embed.set_footer(text=f" | Requested by {ctx.author}.", icon_url=ctx.author.avatar_url)
+        embed.set_author(name=f" | Stats", icon_url=self.bot.user.avatar_url)
 
         await ctx.send(embed=embed)
 
