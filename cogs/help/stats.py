7b0b27b129731feea0647108b915445624fe7b34
Iliya Bolotov <54947380+I-Atlas@users.noreply.github.com>
2020-06-03 22:40:19 +0300

    Update for "stats" command
    Update for "stats" command

--- cogs/help/stats.py
+++ cogs/help/stats.py
@@ -18,13 +18,15 @@ class Stats(commands.Cog):
         number_of_members = str(len(set(self.bot.get_all_members())))
         author_id = '<@271684584863825920>'
 
-        embed = discord.Embed(title=f'{self.bot.user.name} Stats', color=ctx.author.color)
+        embed = discord.Embed(color=ctx.author.color)
 
-        embed.add_field(name='Python version', value=platform.python_version(), inline=False)
-        embed.add_field(name='discord.py version', value=discord.__version__, inline=False)
-        embed.add_field(name='Total guilds', value=number_of_servers, inline=False)
-        embed.add_field(name='Total users', value=number_of_members, inline=False)
-        embed.add_field(name='Bot developers', value=author_id, inline=False)
+        embed.add_field(name='Python version', value=platform.python_version())
+        embed.add_field(name='discord.py version', value=discord.__version__)
+        embed.add_field(name='Total guilds', value=number_of_servers)
+        embed.add_field(name='Total users', value=number_of_members)
+        embed.add_field(name='Bot developers', value=author_id)
+
+        embed.set_image(url=self.bot.user.avatar_url)
 
         embed.set_footer(text=f" | Requested by {ctx.author}.", icon_url=ctx.author.avatar_url)
         embed.set_author(name=f" | Stats", icon_url=self.bot.user.avatar_url)
